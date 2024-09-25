c02b7ed7093c86170dc59057c31c70695bcb278c
Nawaank <mirtindayot@gmail.com>
2024-09-25 14:32:36 +0200

    changement dashboard

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,19 +1,23 @@
-document.querySelectorAll('#dropdownMenu a').forEach(function(item) {
-    item.addEventListener('click', function(event) {
-        event.preventDefault();
-        var statusText = this.textContent;
-        var statusButton = document.getElementById('statusButton');
+document.querySelectorAll('.dropdown').forEach(function(dropdown) {
+    dropdown.querySelectorAll('.dropdown-content a').forEach(function(item) {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            var statusText = this.textContent;
+            
+            // Utiliser closest pour trouver le bouton de statut du ticket actuel
+            var statusButton = dropdown.querySelector('.status-btn');
 
-        // Modifier le texte du bouton
-        statusButton.innerText = statusText + ' ▼';
+            // Modifier le texte du bouton
+            statusButton.innerText = statusText + ' ▼';
 
-        // Modifier la couleur selon le statut sélectionné
-        if (this.dataset.status === 'a-traite') {
-            statusButton.className = 'status-btn red'; // Rouge pour "À Traiter"
-        } else if (this.dataset.status === 'en-cours') {
-            statusButton.className = 'status-btn orange'; // Orange pour "En Cours"
-        } else if (this.dataset.status === 'traite') {
-            statusButton.className = 'status-btn green'; // Vert pour "Traité"
-        }
+            // Modifier la couleur selon le statut sélectionné
+            if (this.dataset.status === 'a-traite') {
+                statusButton.className = 'status-btn red'; // Rouge pour "À Traiter"
+            } else if (this.dataset.status === 'en-cours') {
+                statusButton.className = 'status-btn orange'; // Orange pour "En Cours"
+            } else if (this.dataset.status === 'traite') {
+                statusButton.className = 'status-btn green'; // Vert pour "Traité"
+            }
+        });
     });
 });

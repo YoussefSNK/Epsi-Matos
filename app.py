39ddd7c86d5c50188ae4dd121a3f6d2b4257aa2b
Youssef Fadili <myoussef.fadili@gmail.com>
2024-09-27 03:09:04 +0200

    style: nettoyage

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from controllers.auth_controller import auth_bp
 from controllers.main_controller import main_bp
 from controllers.reservation_controller import reservation_bp
 from controllers.suggestion_controller import suggestion_bp
-from controllers.report_controller import report_bp  # Import du contrôleur report
+from controllers.report_controller import report_bp
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -13,7 +13,7 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(main_bp)
 app.register_blueprint(reservation_bp)
 app.register_blueprint(suggestion_bp)
-app.register_blueprint(report_bp)  # Enregistrement du blueprint report
+app.register_blueprint(report_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
